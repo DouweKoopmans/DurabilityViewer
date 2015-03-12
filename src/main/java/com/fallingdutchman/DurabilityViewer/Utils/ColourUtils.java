1b0599262a7412d07eef367791064a533cbf765c
ItzSkillz <falling.dutchman@gmail.com>
2015-03-12 19:25:56 +0100

    added some javadoc and a foundation for a colour system that would allow users to choose two colours instead of one, probably never gonna add this as a feature though.

--- src/main/java/com/fallingdutchman/DurabilityViewer/Utils/ColourUtils.java
+++ src/main/java/com/fallingdutchman/DurabilityViewer/Utils/ColourUtils.java
@@ -7,6 +7,12 @@ import java.awt.*;
 
 public class ColourUtils
 {
+    /**
+     * @param rgb int
+     * takes a rgb value in the form of an int.
+     * used to convert the current colour (as provided by {@link com.fallingdutchman.DurabilityViewer.Gui.DurabilityViewerConfigPanel}) to an int array which is saved.
+     * @return array of int's representing a R, G and B value.
+     */
     public static int[] CurrentRGB(int rgb)
     {
         int[] RGB = new int[3];
@@ -19,10 +25,19 @@ public class ColourUtils
     }
 
 
+    /**
+     * @param rgb int array
+     *
+     * takes an array of 3 int's representing a R, G and B value.
+     * used to convert the RGB arrays from {@link com.fallingdutchman.DurabilityViewer.LiteModDurabilityViewer} which are used to save the current RGB values.
+     *
+     * @return a java color instance of the given rgb value
+     */
     public static Color RGBConverter(int[] rgb)
     {
         return new Color(rgb[0], rgb[1], rgb[2]);
     }
+
 
     public static int DurColour(ItemStack item)
     {
@@ -32,12 +47,17 @@ public class ColourUtils
             int currentDura = item.getMaxDamage() - item.getItemDamage() + 1;
             float percentage = ((float) currentDura - 1.0F) / (float) item.getMaxDamage();
 
-            rgb[0] = (int) (155 * (1-percentage)) + 100;
-            rgb[1] = (int) (155 * percentage) + 100;
+            rgb[0] = (int) (Delta(100, 255) * (1-percentage)) + 100;
+            rgb[1] = (int) (Delta(255, 100) * percentage) + 100;
             rgb[2] = 100;
 
             return RGBConverter(rgb).getRGB();
         }
         return RGBConverter(LiteModDurabilityViewer.instance.DurColour).getRGB();
+    }
+
+    private static int Delta(int Old, int New)
+    {
+        return Math.abs(New - Old);
     }
 }
--- src/main/java/com/fallingdutchman/DurabilityViewer/Utils/DvUtils.java
+++ src/main/java/com/fallingdutchman/DurabilityViewer/Utils/DvUtils.java
@@ -6,13 +6,12 @@ import net.minecraft.item.ItemStack;
 public class DvUtils
 {
     /**
-     * @param item Minecrart ItemStack that you want to check is in the players inventory or not
-     *
-     * @return if the item is in the players inventory and not in a chest or creative menu.
+     * @param item ItemStack
+     * used to check if the provided item is in the players inventory and not in a chest or creative menu
+     * @return boolean.
      */
     public static boolean inInv(ItemStack item)
     {
         return Minecraft.getMinecraft().thePlayer.inventory.hasItemStack(item);
-
     }
 }
